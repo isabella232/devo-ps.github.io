b0f93c48f44a4f1b1e356a23b186eb57509f75a5
Ronan Berder <hunvreus@gmail.com>
2014-07-30 02:16:22 +0200

    Fixing navigation

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -5,23 +5,13 @@ $(function() {
   var sticky = function(){
     var scroll = $(window).scrollTop();
     if ((scroll + 20) > top) {
-      if ($('#main').hasClass('blog')) {
-        var left = 'auto';
-        var right = 0;
-      }
-      else {
-        var left = 0;
-        var right = 'auto';
-      }
       $('#main .sidebar').addClass('sticky').css({
-        'left': left,
-        'right': right,
+        'right': 0,
         'top': scroll + 20 - main
       });
     }
     else {
       $('#main .sidebar').removeClass('sticky').css({
-        'left': 'auto',
         'right': 'auto',
         'top': 'auto'
       });
