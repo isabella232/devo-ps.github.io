3d8b28b5800c4187e583db79abab64618494bd67
Ronan Berder <hunvreus@gmail.com>
2014-08-24 22:45:16 +0800

    Updating the front page

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -1,25 +1,27 @@
 $(function() {
-  // Make menu sticky on scroll
-  var top = $('#main .sidebar').offset().top;
-  var main = $('#main').offset().top;
-  var sticky = function(){
-    var scroll = $(window).scrollTop();
-    if ((scroll + 20) > top) {
-      $('#main .sidebar').addClass('sticky').css({
-        'right': 0,
-        'top': scroll + 20 - main
-      });
-    }
-    else {
-      $('#main .sidebar').removeClass('sticky').css({
-        'right': 'auto',
-        'top': 'auto'
-      });
-    }
-  };
+  if ($('#main .sidebar').length) {
+    // Make menu sticky on scroll
+    var top = $('#main .sidebar').offset().top;
+    var main = $('#main').offset().top;
+    var sticky = function(){
+      var scroll = $(window).scrollTop();
+      if ((scroll + 20) > top) {
+        $('#main .sidebar').addClass('sticky').css({
+          'right': 0,
+          'top': scroll + 20 - main
+        });
+      }
+      else {
+        $('#main .sidebar').removeClass('sticky').css({
+          'right': 'auto',
+          'top': 'auto'
+        });
+      }
+    };
 
-  sticky();
-  $(window).scroll(function() {
-     sticky();
-  });
+    sticky();
+    $(window).scroll(function() {
+       sticky();
+    });
+  }
 });
---
+++ source/blog/docker-dos-and-donts.md
@@ -0,0 +1,16 @@
+---
+draft: true
+collection: blog
+title: "Docker Do's And Don'ts"
+tags:
+  - Docker
+  - best-practices
+
+author: vincent
+hn: 
+date: 2014-08-23
+
+template: post.html
+---
+
+<script async class='speakerdeck-embed' data-id='9b9300600cbe01326b873a4a8db3af3a' data-ratio='1.33333333333333' src='//speakerdeck.com/assets/embed.js'></script>
